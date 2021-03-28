a6836a87f3af03b2b5e811cb48c80591a6d9cd00
Jerid Fenderson <jrfenderson@gmail.com>
2021-03-27 20:38:22 -0400

    Fairly easy to manipulate, next is to integrate into CarSales

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import SwiperCore, { Navigation, Pagination, Thumbs } from 'swiper'
+import SwiperCore, { Navigation, Pagination, Thumbs, Autoplay } from 'swiper'
 import 'swiper/swiper-bundle.css'
 
-SwiperCore.use([Navigation, Pagination, Thumbs])
+SwiperCore.use([Navigation, Pagination, Thumbs, Autoplay])
 
 export function App() {
   const [thumbsSwiper, setThumbsSwiper] = useState()
@@ -12,7 +12,11 @@ export function App() {
   for (let i = 0; i < 5; i++) {
     slides.push(
       <SwiperSlide key={`slide-${i}`}>
-        <img src={`https://picsum.photos/id/${i}/500/300`} alt={`Slide ${i}`} />
+        <img
+          src={`https://picsum.photos/id/${i}/500/300`}
+          alt={`Slide ${i}`}
+          className="slideImg"
+        />
       </SwiperSlide>
     )
   }
@@ -25,6 +29,7 @@ export function App() {
         <img
           src={`https://picsum.photos/id/${i}/163/100`}
           alt={`Thumbnail ${i}`}
+          className="thumbnail"
         />
       </SwiperSlide>
     )
@@ -39,6 +44,7 @@ export function App() {
         pagination
         spaceBetween={0}
         slidesPerView={1}
+        autoplay
       >
         {slides}
       </Swiper>
@@ -50,6 +56,7 @@ export function App() {
       >
         {thumbs}
       </Swiper>
+      <p>Other Content On Page</p>
     </div>
   )
 }
